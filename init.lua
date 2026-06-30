cf7e51e841f2b4725abdcd5df4990dc33b1c6b91
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-06-29 19:07:11 -0500

    Update and add settings

--- init.lua
+++ init.lua
@@ -3,52 +3,46 @@
 -- Distributed under the LGPLv2.1 (https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)
 
 one_for_all = {}
+
 local mod = one_for_all
-local mod_name = 'one_for_all'
-mod.version = '20220505'
-mod.path = core.get_modpath(core.get_current_modname())
-mod.world = core.get_worldpath()
+local mod_name = "one_for_all"
 
-local xdata = core.get_mod_storage()
+mod.settings.kick = core.settings:get_bool("one_for_all.kick", false)
+mod.settings.perma_kick = core.settings:get_bool("one_for_all.perma_kick", false)
+mod.settings.chat_join_msg = core.settings:get_bool("one_for_all.chat_join_msg", true)
 
-
-local function permakill(player_name)
-	core.kick_player(player_name)
-	core.after(1, function(player_name)
-		core.remove_player(player_name)
-		core.remove_player_auth(player_name)
-	end, player_name)
-	core.log('info', mod_name .. ': permanently killed', player_name)
-
-	local players = xdata:get_string('players') or ''
-	players = players:gsub(',*' .. player_name .. ',*', ',')
-	players = players:gsub(',$', '')
-	xdata:set_string('players', players)
-	--print(players)
+function mod.kill(player_name)
+	local player = core.get_player_by_name(player_name)
+	if player then -- make sure player exists
+		if mod.settings.kick then
+			core.kick_player(player_name)
+			if mod.settings.perma_kick then
+				core.after(1,
+					function()
+						core.remove_player(player_name)
+						core.remove_player_auth(player_name)
+					end,
+					player_name
+				)
+				core.log("info", mod_name .. ": permanently kicked", player_name)
+			end
+		else
+			player:set_hp(0)
+		end
+	end
 end
 
-
-core.register_on_dieplayer(function(player, reason)
-	local players = xdata:get_string('players') or ''
-	if players == '' then
-		return
-	end
-
-	for pl in players:gmatch('[^,]+') do
-		if not core.check_player_privs(pl, 'server') then
-			permakill(pl)
+core.register_on_dieplayer(function()
+	for _, pl in ipairs(core.get_connected_players()) do
+		if not core.check_player_privs(pl, "server") then
+			mod.kill(pl)
 		end
 	end
 end)
 
-
 core.register_on_newplayer(function(player)
-	local players = xdata:get_string('players') or ''
-	local player_name = player:get_player_name()
-	if players == '' then
-		players = player_name
-	else
-		players = players .. ',' .. player_name
+	local pname = player:get_player_name()
+	if mod.settings.chat_join_msg then
+		core.chat_send_player(pname, "If anyone dies then you die too!")
 	end
-	xdata:set_string('players', players)
 end)
